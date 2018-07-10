24e751a29a44035f3fa41b275992bcaf273ad540
huhaoyuan999 <1042379603@qq.com>
2018-07-10 11:41:10 +0800

    测试

---
+++ PracticeOnTheMachine/File/src/aSFHDSK.java
@@ -0,0 +1,11 @@
+public class aSFHDSK {
+    public int fsdhfk = 0;
+    public String sakhfkg = "";
+
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+
+    }
+
+}
