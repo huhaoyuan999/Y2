b726fd9000fbd6f706383f59ee644fa099c04562
huhaoyuan999 <1042379603@qq.com>
2018-07-13 13:20:10 +0800

    第四章练习

---
+++ PracticeOnTheMachine/SMBMS/src/main/java/cn/smbms/tools/myBatisUtil.java
@@ -0,0 +1,42 @@
+package cn.smbms.tools;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class myBatisUtil {
+    private static SqlSessionFactory sqlSessionFactory;
+
+    static {
+        try {
+            InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 打开资源
+     *
+     * @return
+     */
+    public static SqlSession openSession() {
+        return sqlSessionFactory.openSession(false);
+    }
+
+    /**
+     * 释放资源！
+     *
+     * @param sqlSession
+     */
+    public static void closeSession(SqlSession sqlSession) {
+        if (null != sqlSession) {
+            sqlSession.close();
+        }
+    }
+}
